1e2d413510df08a7d883979edf6f7f51de176725
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-04-20 00:06:40 +0530

    firebase: add firebase admin and client setup

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -11,6 +11,9 @@ import Link from "next/link";
 import {toast} from "sonner";
 import FormField from "@/components/FormField";
 import {useRouter} from "next/navigation";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from "@/firebase/client";
+import {signIn, signUp} from "@/lib/actions/auth.actions";
 
 const authFormSchema = (type: FormType) => {
     return z.object({
@@ -33,12 +36,43 @@ const AuthForm = ({ type }: { type: FormType }) => {
         },
     })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    async function onSubmit(values: z.infer<typeof formSchema>) {
         try {
             if(type === 'sign-up') {
+                const { name, email, password } = values;
+
+                const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+
+                const result = await signUp({
+                    uid: userCredentials.user.uid,
+                    name: name!,
+                    email,
+                    password,
+                })
+
+                if(!result?.success) {
+                    toast.error(result?.message);
+                    return;
+                }
+
                 toast.success('Account created successfully. Please sign in.');
                 router.push('/sign-in')
             } else {
+                const { email, password } = values;
+
+                const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+                const idToken = await userCredential.user.getIdToken();
+
+                if(!idToken) {
+                    toast.error('Sign in failed')
+                    return;
+                }
+
+                await signIn({
+                    email, idToken
+                })
+
                 toast.success('Sign in successfully.');
                 router.push('/')
             }
@@ -60,10 +94,10 @@ const AuthForm = ({ type }: { type: FormType }) => {
                         height={32}
                         width={38}
                     />
-                    <h2 className="text-primary-100">MockMate</h2>
+                    <h2 className="text-primary-100">PrepWise</h2>
                 </div>
 
-                <h3>Practice job interview with MockMate Ai</h3>
+                <h3>Practice job interview with AI</h3>
 
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
--- firebase/client.ts
+++ firebase/client.ts
@@ -1,7 +1,7 @@
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyA2IHoAGuJUqPKFBLHMbvC3VzOaoc3xHEk",
@@ -13,6 +13,8 @@ const firebaseConfig = {
   measurementId: "G-078XDV0Y37"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app)
